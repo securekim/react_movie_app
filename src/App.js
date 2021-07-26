6a31e95c92d8132c4e762465a730bb919a43e9b4
securekim <35220663+securekim@users.noreply.github.com>
2021-07-27 01:14:04 +0900

    2.1 Test - Reusable Components with JSX+Props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Potato from "./Potato"
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!!</h1>
+      <Potato />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Potato from './Potato';
 
+//버전업이 되면서 여러개의 Component를 넣을 수 있게 됨 (App, Potato)
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Potato /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//Component : HTML 을 반환하는 함수.
+//JSX : Javascript 내의 html (REACT만의 개념)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

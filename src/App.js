0d4fa892e69b830dad585a1e1a59c6cff2010653
securekim <35220663+securekim@users.noreply.github.com>
2021-07-27 02:02:42 +0900

    2.4 Test - Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,33 @@
+import PropTypes from "prop-types"
+import foodILike from "./Data"
 
-function Food({name, picture}){
+function Food({name, picture, rating}){
   return (
     <div>
     <h2> I like {name} </h2>
+    <h4> Rating : {rating}/5.0</h4>
     <img alt={name} src = {picture}></img>
     </div>
   )
 }
 
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-  }
-];
-
+Food.typeCheck = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number,
+  numberOrString : PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.instanceOf({})
+  ])
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!!</h1>
       {foodILike.map(dish =>(
-        <Food key={dish.name} name={dish.name} picture={dish.image} />
+        <Food key={dish.name} name={dish.name} picture={dish.image} rating={dish.rating}/>
       ))}
     </div>
   );
